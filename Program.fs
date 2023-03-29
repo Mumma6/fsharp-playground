3b07e9d11f741ea1e5f9c20c7c5cbd2ea7833149
Martin <moarte6@gmail.com>
2023-03-29 14:31:54 +0200

    first

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "f# is awesome!!!"
+
+printfn "hejsan"
+
+
+let square x = x * x
+
+let res = square(20)
+
+printfn "%A" res
